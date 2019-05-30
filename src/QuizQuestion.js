2216edffd34d8adbec4db108aa09b66ffe19831e
Jesus <jesus285@live.com.mx>
2019-05-30 14:52:45 -0500

    section
    se corrige etiqueta del section

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,9 +4,9 @@ class QuizQuestion extends Component{
     render(){
         return(
             <main>
-                <secton>
+                <section>
                     <p>{this.props.quiz_question.instruction_text}</p>
-                </secton>
+                </section>
                 <section class="buttons">
                     <ul>
                         <li>{this.props.quiz_question.answer_options[0]}</li>
