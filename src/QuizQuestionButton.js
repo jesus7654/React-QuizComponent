9bb4a9027f4110ede8217b577dcd8add8624acbb
Jesus <jesus285@live.com.mx>
2019-05-30 15:13:17 -0500

    correccion en quiz question button
    se corrige nombre de la clase

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,6 +1,6 @@
 import  React,{Component}  from "react";
 
-class quizQuestionButton extends Component{
+class QuizQuestionButton extends Component{
 
     render(){
         return (
@@ -12,4 +12,4 @@ class quizQuestionButton extends Component{
 
 }
 
-export default quizQuestionButton
+export default QuizQuestionButton;
